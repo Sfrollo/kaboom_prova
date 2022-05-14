2f9d6402d99f4334f52adc7cbd1bb8eb14feeb28
AleDanna <38246018+Sfrollo@users.noreply.github.com>
2022-05-14 16:23:42 +0000

    Score sui frame + game over

--- code/main.js
+++ code/main.js
@@ -4,74 +4,115 @@ import kaboom from "kaboom"
 kaboom()
 
 // load assets
-loadSprite("bean", "sprites/bean.png")
+loadSprite("bean", "sprites/bean.png");
 
-// add a character to screen
-const bean = add([
-	// list of components
-	sprite("bean"),
-  //posizione x y
-	pos(80, 0),
-  //scala
-  scale(1),
-  rotate(0),
-  // color(0, 0, 255),
-  //collider
-	area(),
-  body(),
-])
-//jump on space
-onKeyPress("space", () => {
-  //jump e grounded vengono forniti da body()
-  if(bean.grounded()){
-    bean.jump(); 
-  }
+let score = 0;
+
+
+
+scene("lose", () => {
+	add([
+		text("Game Over"),
+    pos(center()),
+    origin("center")
+	]);
+
+  	add([
+		text(score),
+		pos(width() / 2, height() / 2 + 80),
+		scale(2),
+		origin("center"),
+	]);
+
+	// go back to game with space is pressed
+	onKeyPress("space", () =>{
+    score = 0;
+    go("game")
+  });
+	onClick(() => go("game"));
 })
 
-// add a kaboom on mouse click
-onClick(() => {
-	addKaboom(mousePos())
-})
-
-// burp on "b"
-onKeyPress("b", burp)
-
-//pavimento
-
-add([
-  rect(width(), 48),
-  pos(0, height() - 48),
-  outline(4),
-  //collider
-  area(),
-  //rende l'oggetto inattraversabile
-  solid(),
-  color(127, 200, 255)
-])
-
-//ostacoli
-
-function spawnTree(){
-  add([
-    rect(48, rand(24, 64)),
-    area(),
-    outline(4),
-    pos(width(), height() - 48),
-    origin("botleft"),
-    color(255, 180, 255),
-    move(LEFT, 240),
-    "tree"
+scene("game", () => {
+  const scoreLabel = add([
+     text(score),
+    pos(24, 24)
+  ]);
+  // add a character to screen
+  onUpdate(() => {
+	  score++;
+	  scoreLabel.text = score;
+  });
+  
+  const bean = add([
+	  // list of components
+	  sprite("bean"),
+    //posizione x y
+	  pos(80, 0),
+    //scala
+    scale(1),
+    rotate(0),
+    // color(0, 0, 255),
+    //collider
+	  area(),
+    body(),
   ])
 
-  wait(rand(0.8, 1.5), () => {
-    spawnTree();
-  });
-}
+  onKeyPress("space", () => {
+    //jump e grounded vengono forniti da body()
+    if(bean.grounded()){
+      bean.jump(); 
+    }
+  })
 
-spawnTree();
+  // onClick(() => {
+	 //  addKaboom(mousePos())
+  // })
+
+  //pavimento
+
+  add([
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
+    //collider
+    area(),
+    //rende l'oggetto inattraversabile
+    solid(),
+    color(127, 200, 255)
+  ])
+
+  //ostacoli
+
+  function spawnTree(){
+    add([
+      rect(48, rand(24, 64)),
+      area(),
+      outline(4),
+      pos(width(), height() - 48),
+      origin("botleft"),
+      color(255, 180, 255),
+      move(LEFT, 240),
+      "tree"
+    ])
+
+    wait(rand(0.8, 1.5), () => {
+      spawnTree();
+    });
+  }
+
+  
+  spawnTree();
+
+  bean.onCollide("tree", () => {
+    addKaboom(bean.pos);
+    shake();
+    go("lose")
+  })
+})
+
+
+go("game")
+
+
 
 //onCollide viene dato da area()
-bean.onCollide("tree", () => {
-  addKaboom(bean.pos);
-  shake();
-})
--- dist/game.js
+++ dist/game.js
@@ -2915,51 +2915,78 @@ vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
   // code/main.js
   no();
   loadSprite("bean", "sprites/bean.png");
-  var bean = add([
-    sprite("bean"),
-    pos(80, 0),
-    scale(1),
-    rotate(0),
-    area(),
-    body()
-  ]);
-  onKeyPress("space", () => {
-    if (bean.grounded()) {
-      bean.jump();
-    }
-  });
-  onClick(() => {
-    addKaboom(mousePos());
-  });
-  onKeyPress("b", burp);
-  add([
-    rect(width(), 48),
-    pos(0, height() - 48),
-    outline(4),
-    area(),
-    solid(),
-    color(127, 200, 255)
-  ]);
-  function spawnTree() {
+  var score = 0;
+  scene("lose", () => {
     add([
-      rect(48, rand(24, 64)),
-      area(),
-      outline(4),
-      pos(width(), height() - 48),
-      origin("botleft"),
-      color(255, 180, 255),
-      move(LEFT, 240),
-      "tree"
+      text("Game Over"),
+      pos(center()),
+      origin("center")
     ]);
-    wait(rand(0.8, 1.5), () => {
-      spawnTree();
+    add([
+      text(score),
+      pos(width() / 2, height() / 2 + 80),
+      scale(2),
+      origin("center")
+    ]);
+    onKeyPress("space", () => {
+      score = 0;
+      go("game");
     });
-  }
-  __name(spawnTree, "spawnTree");
-  spawnTree();
-  bean.onCollide("tree", () => {
-    addKaboom(bean.pos);
-    shake();
+    onClick(() => go("game"));
   });
+  scene("game", () => {
+    const scoreLabel = add([
+      text(score),
+      pos(24, 24)
+    ]);
+    onUpdate(() => {
+      score++;
+      scoreLabel.text = score;
+    });
+    const bean = add([
+      sprite("bean"),
+      pos(80, 0),
+      scale(1),
+      rotate(0),
+      area(),
+      body()
+    ]);
+    onKeyPress("space", () => {
+      if (bean.grounded()) {
+        bean.jump();
+      }
+    });
+    add([
+      rect(width(), 48),
+      pos(0, height() - 48),
+      outline(4),
+      area(),
+      solid(),
+      color(127, 200, 255)
+    ]);
+    function spawnTree() {
+      add([
+        rect(48, rand(24, 64)),
+        area(),
+        outline(4),
+        pos(width(), height() - 48),
+        origin("botleft"),
+        color(255, 180, 255),
+        move(LEFT, 240),
+        "tree"
+      ]);
+      wait(rand(0.8, 1.5), () => {
+        spawnTree();
+      });
+    }
+    __name(spawnTree, "spawnTree");
+    spawnTree();
+    bean.onCollide("tree", () => {
+      addKaboom(bean.pos);
+      shake();
+      go("lose");
+    });
+  });
+  go("game");
 })();
 //# sourceMappingURL=game.js.map
